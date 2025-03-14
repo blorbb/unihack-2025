431532e2f87c48ae23a5cb3d1883f035c53b549d
Indra Kusumah-Kasim <shadeditor@gmail.com>
2025-03-15 00:12:11 +1100

    shitty types

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,11 +1,65 @@
 #![feature(try_blocks)]
 #![feature(let_chains)]
 
-use std::sync::atomic::{AtomicU32, Ordering};
+use std::{
+    collections::HashMap,
+    sync::atomic::{AtomicU32, Ordering},
+};
+
+type UnitCode = String;
+type Activity = String;
+type Username = String;
+
+#[derive(Clone, Copy, Hash)]
+enum WeekDay {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+}
+
+#[derive(Clone, Copy, Hash)]
+struct Class {
+    day: WeekDay,
+
+    // Minutes from midnight
+    start: usize,
+    end: usize,
+}
+
+type ClassTimes = HashMap<UnitCode, HashMap<Activity, Vec<Class>>>;
+
+struct Units {
+    class_times: ClassTimes,
+}
+
+struct UnitInfo {
+    name: String,
+}
+
+enum Preference {
+    ShareClass(UnitCode, Activity, Username),
+}
+
+struct UserInfo {
+    units: Vec<UnitCode>,
+    preferences: Vec<Preference>,
+}
+
+pub mod solver {
+    use std::collections::BTreeMap;
+
+    use super::*;
+
+    type Solution = BTreeMap<Username, BTreeMap<UnitCode, BTreeMap<Activity, Class>>>;
+
+    pub fn solve(class_times: &ClassTimes, users: &HashMap<Username, UserInfo>) -> Solution {
+        Solution::new()
+    }
+}
 
 pub fn increment(message: &str) -> u32 {
     static N: AtomicU32 = AtomicU32::new(0);
-
-    println!("got a message on the server: {message}");
     N.fetch_add(1, Ordering::Relaxed)
 }
