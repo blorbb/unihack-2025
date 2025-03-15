f8cf97ceca301075d8ca3d52ce921c0090186ada
azhn <anthony.zh.oon@gmail.com>
2025-03-15 11:35:33 +1100

    Refactor groups

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,73 +1,61 @@
 #![feature(try_blocks)]
 #![feature(let_chains)]
-use std::sync::atomic::{AtomicU32, Ordering};
-use std::{
-    collections::{HashMap, hash_map::Entry},
-    sync::{LazyLock, Mutex},
-};
 
 const TESTING: bool = true;
 
-use serde::{Deserialize, Serialize};
-type Member = String;
-type GroupName = String;
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Group {
-    pub name: GroupName,
-    pub members: Vec<Member>,
-}
-impl Group {
-    const fn new(name: GroupName) -> Self {
-        Self {
-            name,
-            members: vec![],
+pub mod classes;
+
+pub mod groups {
+    use super::TESTING;
+    use serde::{Deserialize, Serialize};
+    use std::{
+        collections::{HashMap, hash_map::Entry},
+        sync::{LazyLock, Mutex},
+    };
+    use uuid::Uuid;
+    type MHashMap<K, V> = Mutex<HashMap<K, V>>;
+    static GROUPS: LazyLock<MHashMap<Uuid, Group>> = LazyLock::new(|| {
+        let mut map = HashMap::<_, _>::new();
+        if TESTING {
+            let mut group = Group::new();
+            group.members.extend(
+                vec!["bobr", "cat", "car"]
+                    .into_iter()
+                    .map(String::from)
+                    .collect::<Vec<_>>(),
+            );
+            map.insert(Uuid::nil(), group);
+        }
+        Mutex::new(map)
+    });
+    type Member = String;
+    #[derive(Debug, Clone, Serialize, Deserialize)]
+    pub struct Group {
+        pub members: Vec<Member>,
+    }
+    impl Group {
+        const fn new() -> Self {
+            Self { members: vec![] }
         }
     }
-}
-type MHashMap<K, V> = Mutex<HashMap<K, V>>;
-static GROUPS: LazyLock<MHashMap<GroupName, Group>> = LazyLock::new(|| {
-    let mut map = HashMap::<_, _>::new();
-    if TESTING {
-        let name = String::from("testid");
-        let mut group = Group::new(name.clone());
-        group.members.extend(
-            vec!["bobr", "cat", "car"]
-                .into_iter()
-                .map(String::from)
-                .collect::<Vec<_>>(),
-        );
-        map.insert(name, group);
+    // TODO: Give a real Error
+    pub fn create_group() -> Result<String, ()> {
+        let id = Uuid::now_v7();
+        let mut groups = GROUPS.lock().unwrap(); // Take lock to access inside
+        match groups.entry(id) {
+            Entry::Occupied(_) => return Err(()),
+            Entry::Vacant(x) => x.insert_entry(Group::new()),
+        };
+        Ok(id.to_string())
     }
-    Mutex::new(map)
-});
 
-pub mod classes;
-pub mod solver;
-
-// TODO(delete):
-pub fn increment(message: &str) -> u32 {
-    static N: AtomicU32 = AtomicU32::new(0);
-
-    println!("got a message on the server: {message}");
-    N.fetch_add(1, Ordering::Relaxed)
-}
-
-// TODO: Give a real Error
-pub fn create_group(name: &str) -> Result<String, ()> {
-    let mut groups = GROUPS.lock().unwrap(); // Take lock to access inside
-    match groups.entry(name.to_string()) {
-        Entry::Occupied(_) => return Err(()),
-        Entry::Vacant(x) => x.insert_entry(Group::new(name.to_string())),
-    };
-    Ok(name.to_string())
-}
-
-pub fn get_group(name: &str) -> Option<Group> {
-    if TESTING {
-        eprintln!("Get group request");
-        dbg!(name);
-        dbg!(GROUPS.lock().unwrap().get(name).cloned())
-    } else {
-        GROUPS.lock().unwrap().get(name).cloned()
+    pub fn get_group(id: Uuid) -> Option<Group> {
+        if TESTING {
+            eprintln!("Get group request");
+            dbg!(id);
+            dbg!(GROUPS.lock().unwrap().get(&id).cloned())
+        } else {
+            GROUPS.lock().unwrap().get(&id).cloned()
+        }
     }
 }
