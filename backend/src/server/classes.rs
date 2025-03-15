3519635a184110e7a5e2f1cbae1287ffbbb69387
Lauren Yim <31467609+cherryblossom000@users.noreply.github.com>
2025-03-15 12:15:27 +1100

    move types into shared

--- backend/src/server/classes.rs
+++ backend/src/server/classes.rs
@@ -2,49 +2,10 @@ use std::{collections::HashMap, path::Path};
 
 use anyhow::{Result, anyhow};
 use itertools::Itertools;
-use num_derive::FromPrimitive;
 use num_traits::FromPrimitive;
 use tokio_stream::{StreamExt, wrappers::ReadDirStream};
 
-pub type UnitCode = String;
-pub type Activity = String;
-pub type Username = String;
-
-#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord, FromPrimitive)]
-pub enum WeekDay {
-    Monday,
-    Tuesday,
-    Wednesday,
-    Thursday,
-    Friday,
-}
-
-#[derive(Clone, Hash, Debug, PartialEq, Eq, PartialOrd, Ord)]
-pub struct Class {
-    pub day: WeekDay,
-    pub code: String,
-
-    /// Minutes from midnight
-    pub start: u16,
-    /// Minutes from midnight
-    pub end: u16,
-}
-
-pub type Classes = HashMap<Activity, Vec<Class>>;
-
-#[derive(Clone, Debug)]
-pub struct UnitInfo {
-    pub name: String,
-}
-
-pub enum Preference {
-    ShareClass(UnitCode, Activity, Username),
-}
-
-pub struct UserInfo {
-    pub units: Vec<UnitCode>,
-    pub preferences: Vec<Preference>,
-}
+use crate::{Activity, Class, Classes, UnitCode, UnitInfo};
 
 fn parse_class(data: &serde_json::Value) -> Option<Option<(Activity, Class)>> {
     let part = data.get("part")?.as_str()?;
