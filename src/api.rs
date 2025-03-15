b15bdc309c1839627014565cee4ea8ec5fd1e4df
blorbb <88137137+blorbb@users.noreply.github.com>
2025-03-15 20:57:19 +1100

    make api return more useful thing

--- src/api.rs
+++ src/api.rs
@@ -1,11 +1,13 @@
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, BTreeSet};
 
 use leptos::prelude::*;
 
 use backend::{
     activity::{Activity, Class, UnitCode},
-    Group, Member,
+    members::Preference,
+    Member,
 };
+use serde::{Deserialize, Serialize};
 
 #[server]
 pub async fn create_group() -> Result<String, ServerFnError> {
@@ -14,8 +16,46 @@ pub async fn create_group() -> Result<String, ServerFnError> {
 }
 
 #[server]
-pub async fn get_group(id: String) -> Result<Option<Group>, ServerFnError> {
-    Ok(backend::api::get_group(&id))
+pub async fn get_group(id: String) -> Result<Option<GroupInfo>, ServerFnError> {
+    let Some(group) = backend::api::get_group(&id) else {
+        return Ok(None);
+    };
+    Ok(Some(GroupInfo {
+        members: group
+            .members
+            .into_iter()
+            .map(|mem| MemberInfo {
+                name: mem.name,
+                units: mem
+                    .units
+                    .into_iter()
+                    .map(|unit| MemberUnitPreferences {
+                        code: unit.clone(),
+                        activities: backend::api::get_activities(&unit)
+                            .expect("unit code should exist")
+                            .into_iter()
+                            .map(|activity| {
+                                (
+                                    activity.clone(),
+                                    mem.preferences
+                                        .iter()
+                                        .filter_map(|pref| match pref {
+                                            Preference::ShareClass(
+                                                punit,
+                                                pactivity,
+                                                share_with,
+                                            ) => (&unit == punit && &activity == pactivity)
+                                                .then_some(share_with.clone()),
+                                        })
+                                        .collect(),
+                                )
+                            })
+                            .collect(),
+                    })
+                    .collect(),
+            })
+            .collect(),
+    }))
 }
 
 #[server]
@@ -25,8 +65,8 @@ pub async fn add_group_member(group_id: String, member: String) -> Result<(), Se
 }
 
 #[server]
-pub async fn update_member(group_id: String, member: Member) -> Result<(), ServerFnError> {
-    backend::api::update_member(&group_id, member)
+pub async fn update_member(group_id: String, member: MemberInfo) -> Result<(), ServerFnError> {
+    backend::api::update_member(&group_id, Member::from(member))
         .map_err(|e| ServerFnError::ServerError(e.to_string()))
 }
 
@@ -48,4 +88,45 @@ pub async fn get_member_calendar(
 #[server]
 pub async fn get_unit_activities(unit: String) -> Result<Option<Vec<String>>, ServerFnError> {
     Ok(backend::api::get_activities(&unit))
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct GroupInfo {
+    pub members: Vec<MemberInfo>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct MemberInfo {
+    pub name: String,
+    pub units: Vec<MemberUnitPreferences>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct MemberUnitPreferences {
+    pub code: UnitCode,
+    /// Map from activity name to people
+    pub activities: BTreeMap<String, BTreeSet<String>>,
+}
+
+impl From<MemberInfo> for Member {
+    fn from(value: MemberInfo) -> Self {
+        Self {
+            name: value.name,
+            units: value.units.iter().map(|u| u.code.clone()).collect(),
+            preferences: value
+                .units
+                .into_iter()
+                .flat_map(|unit| {
+                    unit.activities
+                        .into_iter()
+                        .flat_map(move |(activity, members)| {
+                            let code = unit.code.clone();
+                            members.into_iter().map(move |member| {
+                                Preference::ShareClass(code.clone(), activity.clone(), member)
+                            })
+                        })
+                })
+                .collect(),
+        }
+    }
 }
